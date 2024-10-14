046dff809da21b92336fd972c0ac22fa5bf2c798
shravanibgowda <shravanibyregowda@gmail.com>
2024-10-14 10:23:05 +0530

    Add files via upload

---
+++ labsheet1 (1).R
@@ -0,0 +1,180 @@
+myString <-"Hello,World!"
+print(myString)
+
+v <-TRUE
+print(class(v))
+
+v <-23.5
+print(class(v))
+
+v <-2L
+print(class(v))
+
+v <-2+5i
+print(class(v))
+
+v <-"TRUE"
+print(class(v))
+
+v <-charToRaw("Hello")
+print(class(v))
+
+x = 11
+
+print(x)
+
+x
+[1]11
+
+X
+
+y <- 7
+y
+
+y <- 9
+y
+
+ls()
+ 
+rm(y)
+
+y
+ 
+y <-9
+z <-x+y
+z
+
+x-y
+
+x*y
+
+x/y
+
+y^2
+
+x^2 + y^2
+
+sqrt(y)
+
+y^(1/2)
+
+log(y)
+
+x.1 <- 14
+x.1
+
+1x <-22
+ 
+xx <- "marin"
+xx
+
+yy <-"1"
+yy
+
+11+14
+
+7*9
+
+x
+
+y
+
+x+y
+
+exp(y)
+
+log2(y)
+
+abs(-14)
+
+sqrt(y
++
++  )
+     
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v+t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v-t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v*t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v/t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v%%t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v%/%t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v^t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v>t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v<t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v==t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v<=t)
+
+v <-c(2,5.5,6)
+t <-c(8,3,4)
+print(v>=t)
+
+v <-c(3,1,TRUE,2+3i)
+t <-c(4,1,FALSE,2+3i)
+print(v&t)
+
+v <-c(3,0,TRUE,2+2i)
+t <-c(4,0,FALSE,2+3i)
+print(v|t)
+
+v <-c(3,0,TRUE,2+2i)
+print(!v)
+
+v <-c(3,0,TRUE,2+2i)
+t <-c(1,3,TRUE,2+3i)
+print(v&&t)
+
+v <-c(0,0,TRUE,2+2i)
+t <-c(0,3,TRUE,2+3i)
+print(v||t)
+
+v1 <- c(3,1,TRUE,2+3i)
+v2 <<- c(3,1,TRUE,2+3i)
+v3 = c(3,1,TRUE,2+3i)
+print(v1)
+print(v2)
+print(v3)
+
+c(3,1,TRUE,2+3i) -> v1
+c(3,1,TRUE,2+3i)  ->> v2
+print(v1)
+print(v2)
+
+v <-2:8
+print(v)
+
+v1 <- 8
+v2 <- 12
+t <- 1:10
+print(v1 %in% t)
+print(v2 %in% t)
